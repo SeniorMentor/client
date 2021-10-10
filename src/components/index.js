be4f337e5d21b261bf8c70dd7d96c21c1781c650
Sudheer Tripathi <tripathisudheer604@gmail.com>
2021-10-10 16:15:31 +0530

    chore: add search bar and menu icons

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as ActivityCard } from './ActivityCard/ActivityCard'
+export { default as ActivityCardLeft } from './ActivityCardLeft/ActivityCardLeft'
+export { default as PostForm } from './PostForm/PostForm'
